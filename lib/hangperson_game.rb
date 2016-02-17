29844ffc3b003d1090de4f39fb77fb4b70592339
sirclesam1 <sirclesam@gmail.com>
2016-02-17 01:05:40 +0000

    Getting along

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,7 +1,40 @@
 class HangpersonGame
-
+  attr_accessor :word,:guesses, :wrong_guesses
+  
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
+
+
+  
+  public
+  def guess(letter)
+    letter_regex = /^[a-zA-Z]$/
+    
+    unless letter =~ letter_regex
+      raise ArgumentError 
+    end
+    
+    
+    letter.downcase!
+
+    if @word.include?(letter)
+      unless @guesses.include?(letter)
+        @guesses = @guesses + letter 
+        return true
+      else
+        return false
+      end
+    else
+      unless @wrong_guesses.include?(letter)
+        @wrong_guesses = @wrong_guesses + letter
+        return true
+      else
+        return false
+      end
+    end
+  end
+    
+    
 
   # Get a word from remote "random word" service
 
@@ -9,7 +42,9 @@ class HangpersonGame
   # end
   
   def initialize(word)
-    @word = word
+    @word = word.downcase
+    @guesses = ""
+    @wrong_guesses = ""
   end
 
   def self.get_random_word
